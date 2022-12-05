e1ea4df50372be9c9c8a2163cf3951e03ad0d3e4
Amine Benhebba <amine.benhebba.ing@gmail.com>
2022-12-06 00:09:22 +0100

    Add Main UI For The Editor
    - Adds Main Menus.
    - Adds Main Toolbars.
    - Add Folders Tree.
    - Adds Main Text Editor
    - Adds Console display Editor.

--- src/CodeEditor.Wpf/Views/MainView.xaml.cs
+++ src/CodeEditor.Wpf/Views/MainView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace CodeEditor.Wpf.Views
 {
@@ -7,6 +8,49 @@ namespace CodeEditor.Wpf.Views
         public MainView()
         {
             InitializeComponent();
+        }
+
+        public void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        public void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                }
+            }
+        }
+
+        private void FileTreeChecked(object sender, RoutedEventArgs e)
+        {
+            TreeViewExpander.IsExpanded = true;
+        }
+
+        private void FileTreeUnchecked(object sender, RoutedEventArgs e)
+        {
+            TreeViewExpander.IsExpanded = false;
+        }
+
+        private void ConsoleChecked(object sender, RoutedEventArgs e)
+        {
+            ConsoleExpander.IsExpanded = true;
+        }
+
+        private void ConsoleUnchecked(object sender, RoutedEventArgs e)
+        {
+            ConsoleExpander.IsExpanded = false;
         }
     }
 }
