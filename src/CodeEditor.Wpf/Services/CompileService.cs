d68538672519f1e1a4de91f022d644f4d016c64e
Amine Benhebba <amine.benhebba.ing@gmail.com>
2022-12-07 01:31:34 +0100

    Add The Basic Errors Messages For The Console To Display.

--- src/CodeEditor.Wpf/Services/CompileService.cs
+++ src/CodeEditor.Wpf/Services/CompileService.cs
@@ -1,11 +1,22 @@
-﻿namespace CodeEditor.Wpf.Services
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System.Linq;
+using System.Text;
+
+namespace CodeEditor.Wpf.Services
 {
     public class CompileService : ICompileService
     {
         public string CheckProgram(string program)
         {
             // Roslyn checks for Errors on the code
-            throw new System.NotImplementedException();
+            var syntaxTree = CSharpSyntaxTree.ParseText(program);
+            var diagnostics = syntaxTree.GetDiagnostics().ToList();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var d in diagnostics)
+            {
+                stringBuilder.AppendLine($"{d.Severity.ToString()}[{d.Id}] : Ln:{d.Location.GetLineSpan().StartLinePosition.Line+1} Ch:{d.Location.GetLineSpan().EndLinePosition.Character}. {d.GetMessage()}");
+            }
+            return stringBuilder.ToString();
         }
     }
 }
